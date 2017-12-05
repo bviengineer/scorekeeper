e58e2404715383841c0148402ada6dacf03b921e
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-12-05 00:02:55 -0500

    added conditional statments to function and another variable that holds the highest possible score

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ var p1ScoreBoard = document.getElementById("p1-score"),
     p2ScoreBoard = document.getElementById("p2-score"),
     p1ScoreBtn = document.getElementById("p1-score-button"),
     p2ScoreBtn = document.getElementById("p2-score-button"),
-    resetScoreBtn = document.getElementById("reset-button");
-    p1Score = 0;
-    P2Score = 0;
+    resetScoreBtn = document.getElementById("reset-button"),
+    p1Score = 0,
+    P2Score = 0,
+    winningScore = 5;
 
     p1ScoreBtn.addEventListener("click", function(){
-        p1Score++;
-        console.log(p1Score);
-        p1ScoreBoard.innerHTML = p1Score;
+        if(p1Score < winningScore){
+            p1Score++;
+            p1ScoreBoard.textContent = p1Score;
+            console.log(p1ScoreBoard);
+        } else if(p1Score = winningScore){
+            p1ScoreBoard.style.color = "red";
+            p1ScoreBtn.style.display = "none";
+            p2ScoreBtn.style.display = "none";
+        }
     });
