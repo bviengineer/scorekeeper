1eee7fd5df54422abf78d434e5f58f3057fd74bf
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-12-04 23:42:09 -0500

    added code to js file and added span tags to numbers in h1

---
+++ script.js
@@ -0,0 +1,14 @@
+var plScoreBoard = document.getElementById("p1-score"),
+    p2ScoreBoard = document.getElementById("p2-score"),
+    p1ScoreBtn = document.getElementById("p1-score-button"),
+    p2ScoreBtn = document.getElementById("p2-score-button"),
+    resetScoreBtn = document.getElementById("reset-button");
+    p1Score = 0;
+    P2Score = 0;
+
+    p1ScoreBtn.addEventListener("click", function(){
+        p1Score++;
+        console.log(p1Score);
+        p1ScoreBoard.innerHTML = p1Score;
+
+    });
